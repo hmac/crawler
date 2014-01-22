0beedb1085d775c63f4858003063fa609fb5eb46
Harry Maclean <harryjmaclean@googlemail.com>
2014-01-22 14:10:04 +0000

    Convert to class structure

--- crawler.rb
+++ crawler.rb
@@ -1,35 +1,41 @@
 require "./crawler_base"
 
-def crawl
-  url = $queue.shift
-  return if url.nil?
-  if not $visited.include?(url)
+class Crawler
+  def initialize(domain)
+    @domain = domain
+    @url = "/"
+    @pages = {}
+    @visited = []
+    @queue = [@url]
+    parsed_url = URI.parse(@domain)
+    @conn = Net::HTTP.new(parsed_url.host, parsed_url.port)
+    @conn.use_ssl = true if @domain =~ /^https/
+  end
+
+  def crawl(url=nil)
+    url ||= @url
+    return if url.nil? or @visited.include?(url)
     puts url
-    $visited.push(url)
+    @visited.push(url)
 
     page = page(url)
     links = links_on_page(page)
     links = links.select { |l| local_link?(l) }.map { |l| make_local(l) }
-    links.map { |l| $queue.push(l) }
 
     # Non-html assets
     assets = assets_on_page(page)
-    $pages[url] = assets
+    @pages[url] = assets
 
-    
+    links.each { |l| crawl(l) }
   end
-  crawl()
-end
 
-def create_node(link, html)
-  node = {url: link, html: html}
-  return node
-end
+  def create_node(link, html)
+    node = {url: link, html: html}
+    return node
+  end
 
-def start(domain, url)
-  $pages = {}
-  $visited = []
-  $domain = domain
-  $queue = [url]
-  crawl()
+  def pages
+    @pages
+  end
+
 end
--- crawler_base.rb
+++ crawler_base.rb
@@ -3,46 +3,46 @@ require "nokogiri"
 
 # Currently limits to subdomain e.g. google.com will not reach www.google.com
 
+class Crawler
 
-def local_link?(url)
-  # If url starts with /, it's local
-  # If it starts with the chosen domain, it's local
-  # If it doesn't contain ://, it's local (e.g. "home.html")
-  if url[0] == "/" or url.match(Regexp.new("^"+$domain)) or !url.match(/:\/\//)
-    true
-  else
-    false
+  def local_link?(url)
+    # If url starts with /, it's local
+    # If it starts with the chosen domain, it's local
+    # If it doesn't contain ://, it's local (e.g. "home.html")
+    if url[0] == "/" or url.match(Regexp.new("^"+@domain)) or !url.match(/:\/\//)
+      true
+    else
+      false
+    end
   end
-end
 
-def make_local(url)
-  # Remove http://domain.tld part
-  url = url.gsub(Regexp.new("^"+$domain), "")
-  # Remove querystring
-  url = url.gsub(/\?.*/, "")
-  # add / if not present
-  if url && url[0] != "/"
-    url = "/"+url
+  def make_local(url)
+    # Remove http://domain.tld part
+    url = url.gsub(Regexp.new("^"+@domain), "")
+    # Remove querystring
+    url = url.gsub(/\?.*/, "")
+    # add / if not present
+    if url && url[0] != "/"
+      url = "/"+url
+    end
+    return url
   end
-  return url
-end
 
-def links_on_page(page)
-  page.css("a").map { |a| a.attributes["href"] && a.attributes["href"].value }.compact.uniq
-end
+  def links_on_page(page)
+    page.css("a").map { |a| a.attributes["href"] && a.attributes["href"].value }.compact.uniq
+  end
 
-def assets_on_page(page)
-  css = page.css("link").map { |l| l.attributes["href"] && l.attributes["href"].value }.compact.uniq
-  scripts = page.css("script").map { |l| l.attributes["src"] && l.attributes["src"].value }.compact.uniq
-  images = page.css("img").map { |l| l.attributes["src"] && l.attributes["src"].value }.compact.uniq
-  scripts+css+images
-end
+  def assets_on_page(page)
+    css = page.css("link").map { |l| l.attributes["href"] && l.attributes["href"].value }.compact.uniq
+    scripts = page.css("script").map { |l| l.attributes["src"] && l.attributes["src"].value }.compact.uniq
+    images = page.css("img").map { |l| l.attributes["src"] && l.attributes["src"].value }.compact.uniq
+    scripts+css+images
+  end
 
-def page(url)
-  parsed_url = URI.parse($domain + url)
-  http = Net::HTTP.new(parsed_url.host, parsed_url.port)
-  http.use_ssl = true if $domain =~ /^https/
-  req = Net::HTTP::Get.new(parsed_url.request_uri)
-  res = http.request(req)
-  Nokogiri::HTML(res.body)
+  def page(url)
+    req = Net::HTTP::Get.new(url)
+    res = @conn.request(req)
+    Nokogiri::HTML(res.body)
+  end
+
 end
