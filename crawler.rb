fea034534c34de61e7ee85e9f0448ae60798d7cc
Harry Maclean <harryjmaclean@googlemail.com>
2014-11-18 17:31:19 +0000

    store links to be crawled in a queue and use a loop to keep stack size constant

--- crawler.rb
+++ crawler.rb
@@ -9,22 +9,28 @@ class Crawler
     parsed_url = URI.parse(@domain)
     @conn = Net::HTTP.new(parsed_url.host, parsed_url.port)
     @conn.use_ssl = true if @domain =~ /^https/
+    @queue = []
   end
 
   def crawl(url=nil)
     url ||= @url
-    return if url.nil? or @pages.keys.include?(url)
-    puts "crawling #{url}"
+    @queue = [url]
 
-    page = page(url)
-    links = links_on_page(page)
-    links = links.select { |l| local_link?(l) }.map { |l| make_local(l) }
+    while @queue.length > 0
+      url = @queue.shift
+      next if url.nil? or @pages.keys.include?(url)
+      puts "crawling #{url}"
 
-    # Non-html assets
-    assets = assets_on_page(page)
-    @pages[url] = assets
+      page = page(url)
+      links = links_on_page(page)
+      links = links.select { |l| local_link?(l) }.map { |l| make_local(l) }
 
-    links.each { |l| crawl(l) }
+      # Non-html assets
+      assets = assets_on_page(page)
+      @pages[url] = assets
+
+      @queue += links
+    end
   end
 
   def create_node(link, html)
